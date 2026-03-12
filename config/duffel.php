328d2d7e55693668220c35c615627ce6e726ed3b
Habib Ahmad <habib@eits.ae>
2026-03-12 14:54:20 +0500

    env updated

--- config/duffel.php
+++ config/duffel.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'       => true,
-    'token'         => '',   // Add your Duffel token here
-    'base_url'      => 'https://api.duffel.com',
-    'api_version'   => 'v2',
-    'timeout'       => 20,
-    'default_limit' => 20,
+    'enabled'       => env('DUFFEL_ENABLED', true),
+    'token'         => env('DUFFEL_TOKEN', ''),
+    'base_url'      => env('DUFFEL_BASE_URL', 'https://api.duffel.com'),
+    'api_version'   => env('DUFFEL_API_VERSION', 'v2'),
+    'timeout'       => env('DUFFEL_TIMEOUT', 20),
+    'default_limit' => env('DUFFEL_DEFAULT_LIMIT', 20),
 ];
--- config/travolyo_b2b.php
+++ config/travolyo_b2b.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
-    'base_url'      => 'https://travolyob2b.com/',
-    'auth_api_key'  => '',
-    'timeout'       => 20,
-    'default_limit' => 20,
-
+    'base_url'      => env('TRAVOLYO_B2B_BASE_URL', 'https://travolyob2b.com/'),
+    'auth_api_key'  => env('TRAVOLYO_B2B_API_KEY', ''),
+    'timeout'       => env('TRAVOLYO_B2B_TIMEOUT', 20),
+    'default_limit' => env('TRAVOLYO_B2B_DEFAULT_LIMIT', 20),
 ];
