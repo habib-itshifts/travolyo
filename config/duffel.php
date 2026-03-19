00a686f916451c38896cf593d67db4d5bf654675
Habib Ahmad <habib@eits.ae>
2026-03-19 13:16:35 +0500

    dufeel fixed

--- config/duffel.php
+++ config/duffel.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'       => env('DUFFEL_ENABLED', true),
+    'enabled'       => true,
     'token'         => env('DUFFEL_TOKEN', ''),
-    'base_url'      => env('DUFFEL_BASE_URL', 'https://api.duffel.com'),
-    'api_version'   => env('DUFFEL_API_VERSION', 'v2'),
-    'timeout'       => env('DUFFEL_TIMEOUT', 20),
-    'default_limit' => env('DUFFEL_DEFAULT_LIMIT', 20),
+    'base_url'      => 'https://api.duffel.com/',
+    'api_version'   => 'v2',
+    'timeout'       => 20,
+    'default_limit' => 20,
 ];
