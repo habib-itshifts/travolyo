2262e1174caf6852e995302d0bc1e91d22fb4c27
Habib Ahmad <habib@eits.ae>
2026-04-03 16:35:15 +0500

    api auth token fixed

--- Modules/Flight/routes/api.php
+++ Modules/Flight/routes/api.php
@@ -6,7 +6,7 @@ use Modules\Flight\Http\Controllers\Api\FlightController;
 Route::prefix('flights')->group(function () {
     Route::post('/search',             [FlightController::class, 'search'])->name('flights.search');
     Route::post('/prebook',            [FlightController::class, 'prebook'])->name('flights.prebook');
-    Route::post('/checkout',           [FlightController::class, 'checkout'])->name('flights.checkout')->middleware('auth:sanctum');
+    Route::post('/checkout',           [FlightController::class, 'checkout'])->name('flights.checkout')->middleware('auth:sanctum,web');
     Route::post('/pay',                [FlightController::class, 'pay'])->name('flights.pay');
     Route::get('/orders/{orderId}',    [FlightController::class, 'order'])->name('flights.order');
     Route::delete('/orders/{orderId}', [FlightController::class, 'cancel'])->name('flights.cancel');
